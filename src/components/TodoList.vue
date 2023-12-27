59b548b3915dd3d3e026614c30c7eccfedc3e799
Artem Voropaev <voropaev.a@tm.biz-apps.ru>
2023-12-27 20:07:13 +0400

    TASK-dev-todo-list create todo list, fetch todos from jsonplaceholder, add delete and patch methods

---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1 class="text-2xl font-bold text-center mb-4">TodoList</h1>
+  <TodoLegend class="mb-4" />
+  <AppLoader v-if="loading" />
+  <ul class="grid sm:grid-cols-2 gap-4" :class="{ blur: loading, 'opacity-75': loading }">
+    <template v-for="todo in todos" :key="todo.id">
+      <li
+        v-if="todo.title"
+        class="flex items-center gap-2 text-white rounded-lg border-0 py-3 px-4 sm:text-sm sm:leading-6 cursor-pointer h-full"
+        :class="statusBackgroundClass(todo)"
+        @dblclick="changeStatus(todo.id, todo.completed)"
+      >
+        <span class="pointer-events-none select-none">{{ todo.title }}</span>
+        <AppButton class="ml-auto bg-white" @click="deleteTodo(todo.id)">
+          <Icon :class="{ 'fill-emerald-500': !todo.completed, 'fill-sky-900': todo.completed }" />
+        </AppButton>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+import AppButton from '@/components/UI/AppButton.vue'
+import TodoLegend from '@/components/TodoLegend.vue'
+import AppLoader from '@/components/AppLoader.vue'
+import Icon from '@/components/UI/AppIcon.vue'
+
+defineProps({
+  todos: {
+    type: Array,
+    default: () => []
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emits = defineEmits(['delete-todo', 'update-todo'])
+
+const statusBackgroundClass = (todo) => {
+  return todo.completed ? 'bg-sky-900' : 'bg-emerald-500'
+}
+
+const deleteTodo = (id) => {
+  emits('delete-todo', id)
+}
+const changeStatus = (id, status) => {
+  emits('update-todo', id, status)
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="text-white font-bold py-1 px-2 min-w-8 rounded">
+    <slot></slot>
+  </button>
+</template>
